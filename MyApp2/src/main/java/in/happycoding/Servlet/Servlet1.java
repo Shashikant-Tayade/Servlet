30d8e96ce156f3d6aaa9fe61bc19347c8db93bdb
Shashikant-Tayade <70316712+Shashikant-Tayade@users.noreply.github.com>
2021-09-04 15:39:12 +0530

    Add files via upload

---
+++ MyApp2/src/main/java/in/happycoding/Servlet/Servlet1.java
@@ -0,0 +1,29 @@
+package in.happycoding.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter writer = response.getWriter();
+		writer.write("<h1> WELCOME TO INDIA </h1>");
+		writer.close();
+	
+	}
+
+	
+
+}
---
+++ MyApp2/src/main/java/in/happycoding/Servlet/Servlet3.java
@@ -0,0 +1,26 @@
+package in.happycoding.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Servlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter writer = response.getWriter();
+		writer.write("<h3> WELCOME TO SERVLET3 </h3>");
+		writer.close();
+	
+	}
+
+}
